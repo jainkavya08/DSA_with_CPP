62ad78bff7a0bf7bf0ef36107695fab392fb9348
jainkavya08 <jkavya865@gmail.com>
2024-12-31 19:13:07 +0530

    Basic I/O

---
+++ Basics/01_learncpp.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>                 //all library included
+using namespace std;             
+// #include <math.h>               //math library
+// #include <string>               //string library
+
+// Output
+// int main() {
+//     cout << "Hey Kavya!"<<endl;
+//     cout << "Hey fucker!";
+//     return 0;
+// }
+
+
+// input
+
+int main () {
+    int x , y;
+    cout << "Enter the value of x ";
+    cin >> x;
+    cout <<"Value of x is : " << x ;
+}
