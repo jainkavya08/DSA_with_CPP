2b47df5df6451ba035bf6f98492d7fbd4715b563
jainkavya <jkavya85@gmail.com>
2025-02-13 06:06:47 +0530

    thurs feb 13

---
+++ Basics/Learncpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//#include <math.h> // for mathematical functions
+//#include <string> // for a string
+
+int main () 
+{
+    std::cout <<"Hey Kavya !" <<std::endl << " Hey Raj! " << std::endl;
+    std::cout <<"Hey Kavya !" <<std::endl;
+    return 0;
+}
