cd93a7fc8e117489475c4a396cca643e42eba67c
jainkavya <jkavya85@gmail.com>
2025-02-25 05:06:40 +0530

    if-else

--- Basics/03_Ifelse.cpp
+++ Basics/03_Ifelse.cpp
@@ -7,15 +7,19 @@ int main ()
     int age , marks , num;
 
     /********** SIMPLE IF-ElSE statement (Checking Voting Eligibility)*****************/
+    
     cout <<"Enter your age : " ;  //Take user input
     cin >> age;  
 
-    if (age >= 18 ) {
+    if (age >= 18 ) {      //If age is 18 or more
         cout << "You are Eligible to vote " << endl;
     }
-    else {
+    else {          // If age is less than 18
         cout << " You are not Eligibile to vote " << endl;
     }
 
+    cout << "-----------------------------------" << endl;
+
+    
 
 }
